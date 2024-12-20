e0da929c158bd1aa7d516cf9373b3e38bb9c7d01
Tamikaya <tamikaya111@gmail.com>
2024-12-20 14:10:13 +0500

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,17 @@
+from fake_math import divide as df
+from true_math import divide as dt
+res1 = df(69, 3)
+
+res2 = df(3, 0)
+
+res3 = dt(49, 7)
+
+res4 = dt(15, 0)
+
+print(res1)
+
+print(res2)
+
+print(res3)
+
+print(res4)
